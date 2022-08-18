0309c2b7842a592ff675a6251787b2a72560c267
morozov1982 <morozov1982@mail.com>
2022-08-18 16:51:46 +0600

    add: lesson_01 homework

--- flask_blog/main/routes.py
+++ flask_blog/main/routes.py
@@ -7,4 +7,9 @@ main = Blueprint('main', __name__)
 @main.route('/')
 @main.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
+
+
+@main.route('/contacts')
+def contacts():
+    return render_template('contact.html')
