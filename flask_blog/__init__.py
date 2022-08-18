9d8a8a1d17cf5ae105151ee6d27df7ed11ba961a
morozov1982 <morozov1982@mail.com>
2022-08-18 15:32:41 +0600

    add: lesson_01_2

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+from flask_blog.config import Config
+
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 def create_app():
     print(__name__)
     app = Flask(__name__)
+    login_manager.init_app(app)
+
+    from flask_blog.main.routes import main
+    app.register_blueprint(main)
+
+    app.config.from_object(Config)
+
     return app
--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from flask_blog import create_app
 
-
 app = create_app()
 
 if __name__ == '__main__':
